5fa8709b89e14869f87f87db21dd37a6014430e7
John Doe <wyqge@MSI>
2024-11-04 16:18:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignments_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls enter your name: ");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine("Pls enter your age: ");
+            double myAge = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pls enter your address: ");
+            string myAddress = Console.ReadLine();
+
+            Console.WriteLine("Name: " + myName);
+            Console.WriteLine("Age: " + myAge);
+            Console.WriteLine("Address: " + myAddress);
+        }
+    }
+}
