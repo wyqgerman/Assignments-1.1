ce45e457ec4d5a59af4e864c449a9406d77613d1
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-04 16:42:03 -0500

    1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //Create a simple console application and store your name, age (should take decimal), address in a string variable and print them back on console. Prompt the user to each time enter name, age and address.
             Console.WriteLine("Pls enter your name: ");
             string myName = Console.ReadLine();
 
@@ -16,6 +17,13 @@
             Console.WriteLine("Name: " + myName);
             Console.WriteLine("Age: " + myAge);
             Console.WriteLine("Address: " + myAddress);
+
+            //Create a simple console application and declare basic datatypes like int, float, decimal, double and print their min and max values.
+
+            //Write a C# Sharp program to print the sum of two numbers.
+
+            //Write a C# Sharp program to print the result of dividing two numbers.
         }
+
     }
 }
